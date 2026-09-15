df1332c723e30ad709ef79458d008873864196fe
AlexFilonenko88 <af88_88@mail.ru>
2026-09-15 15:36:50 +0300

    Fix

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'is_published', 'created_at', 'updated_at')
+
+    list_filter = ('is_published', 'created_at', 'updated_at')
+
+    search_fields = ('title', 'content')
+
+    list_display_links = ('title',)
+
+    # prepopulated_fields = {'slug': ('title',)}
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Текст статьи")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
